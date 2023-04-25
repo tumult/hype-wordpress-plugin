1a1a9190dac10a78198f52fba285c81bf4d2132d
Daniel <50146+themorgantown@users.noreply.github.com>
2023-04-25 14:09:29 -0400

    improve uninstall to handle residual files

--- uninstall.php
+++ uninstall.php
@@ -1,25 +1,36 @@
-<?
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) 
+<?php
+if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit();
+}
 
 global $wpdb;
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}hypeanimations" );
 
-delete_option( 'hypeanimations_db_version' );
+// Remove the hypeanimations table
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}hypeanimations");
 
-function hyperrmdir($dir) {
-	if (is_dir($dir)) {
-		$objects = scandir($dir);
-		foreach ($objects as $object) {
-			if ($object != "." && $object != "..") {
-				if (filetype($dir."/".$object) == "dir") hyperrmdir($dir."/".$object); else unlink($dir."/".$object);
-			}
-		}
-		reset($objects);
-		rmdir($dir);
-	}
-} 
-$upload_dir = wp_upload_dir();
-$anims_dir=$upload_dir['basedir'].'/hypeanimations/';
-hyperrmdir($anims_dir);
+// Remove the 'hypeanimations_db_version' option
+delete_option('hypeanimations_db_version');
+
+// Remove the wp-content/uploads/hypeanimations folder and all its contents
+function removeHypeAnimationsDir($dir)
+{
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object !== "." && $object !== "..") {
+                $filePath = $dir . "/" . $object;
+                if (is_dir($filePath)) {
+                    removeHypeAnimationsDir($filePath);
+                } else {
+                    unlink($filePath);
+                }
+            }
+        }
+        rmdir($dir);
+    }
+}
+
+$uploadDir = wp_upload_dir();
+$animsDir = $uploadDir['basedir'] . '/hypeanimations/';
+removeHypeAnimationsDir($animsDir);
 ?>
